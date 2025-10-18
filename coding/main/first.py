534e185d22186fb312f5ff53eae740b21892ca1f
bluelobsta778 <xbox3602254@gmail.com>
2025-10-18 17:25:45 +0300

    Вывод окна с Hello world

--- coding/main/first.py
+++ coding/main/first.py
@@ -1,1 +1,17 @@
-print("Hello world")
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.uix.label import Label
+
+# Настройки окна
+Window.size = (360, 640)
+Window.clearcolor = (0/255, 0/255, 0/255, 1)  # Черный фон
+Window.title = "Мое приложение"
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text = 'Hello world')
+
+if __name__ == '__main__':
+    MyApp().run()
